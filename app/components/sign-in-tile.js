b94d5a3a00f83f3e91a86b71668aa5ef01bb7aa8
Keely Glenn and Shruti Priya <keely+shruti@>
2017-03-23 11:20:42 -0700

    add thumbnail for the individual page and commit just inc case

--- app/components/sign-in-tile.js
+++ app/components/sign-in-tile.js
@@ -3,18 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isSignUpShowing: false,
   isLogInShowing: false,
-  logInShows: true,
-  signInShows: true,
-  actions: {
 
+  actions: {
     signUpShow: function() {
       this.set('isSignUpShowing', true);
-
-      this.toggleProperty('logInShows');
     },
     logInShow: function() {
       this.set('isLogInShowing', true);
-      this.toggleProperty('signInShows');
-    }
+    },
   }
 });
