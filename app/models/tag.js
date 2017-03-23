81b3041085981781147998a81fcb1b1db728e585
Keely Glenn and Shruti Priya <keely+shruti@>
2017-03-23 15:40:09 -0700

    edit pictures size

--- app/models/tag.js
+++ app/models/tag.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
- name: DS.attr(),
- post: DS.belongsTo('post', { async: true })
+  name: DS.attr(),
+  post: DS.belongsTo('post', { async: true })
 });
